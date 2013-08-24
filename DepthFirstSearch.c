99bf877a26923d4f62ceede7edb6f5cd067ac51a
yangbinqi <ybq@outlook.com>
2013-08-24 00:41:05 -0700

    Depth First Search

--- DepthFirstSearch.c
+++ DepthFirstSearch.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAX_ROW 5
 #define MAX_COL 5
-struct point{int x;int y;} stack[512];
+struct point{int row;int col;} stack[512];
 int top=0;
 void push(struct point p){
   stack[top++]=p;
@@ -44,5 +44,49 @@ void visit_maze(int row,int col,struct point pre){
   push(visit_maze);
 }
 int main(void){
-  maze_print();
+  struct point p={0,0};
+  struct point current;
+  push(p);
+  maze[p.row][p.col]=2;
+  while(!is_empty()){
+    current=pop();
+    if(current.row==MAX_ROW-1&&current.col==MAX_COL-1){
+      break;
+    }
+    //up
+    if(current.row-1>=0&&maze[current.row-1][current.col]==0){
+      push(current);
+      visit_maze(current.row-1,current.col,current);
+      maze_print();
+    }
+    //right
+    if(current.col+1<MAX_COL&&maze[current.row][current.col+1]==0){
+      push(current);
+      visit_maze(current.row,current.col+1,current);
+      maze_print();
+    }
+    //down
+    if(current.row+1<MAX_ROW&&maze[current.row+1][current.col]==0){
+      push(current);
+      visit_maze(current.row+1,current.col,current);
+      maze_print();
+    }
+    //left
+    if(current.col-1>=0&&maze[current.row][current.col-1]==0){
+      push(current);
+      visit_maze(current.row,current.col+1,current);
+      maze_print();
+    }
+  }
+  if(current.row==MAX_ROW-1&&current.col==MAX_COL-1){
+    printf("%d  %d\n",current.row,current.col);
+    
+    while(predecessor[current.row][current.col].row!=-1){
+      current=predecessor[current.row][current.col];
+      printf("%d  %d\n",current.row,current.col);
+    }
+  }
+  else{
+    printf("No path.\n");
+  }
 }
