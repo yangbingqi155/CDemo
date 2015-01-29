5b5443dce33269c0b90b1c3b85bb4ca8857bec73
yangbinqi <ybq@outlook.com>
2015-01-29 16:51:14 +0800

    print hello word for InputOutput Project

---
+++ InputOutput/InputOutput/InputOutput.cpp
@@ -0,0 +1,13 @@
+// InputOutput.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello world.");
+	getchar();
+	return 0;
+}
+
